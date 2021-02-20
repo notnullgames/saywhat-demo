fa8ff67d3222517d3ae9a396205db3ae44e5de56
David Konsumer <konsumer@jetboystudio.com>
2021-02-19 23:30:52 -0800

    improve publish

--- publish.sh
+++ publish.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  GODOT="/Applications/Godot.app/Contents/MacOS/Godot"
+else
+  GODOT=$(which godot)
+fi
+
+mkdir -p builds/html5/
+$GODOT --export HTML5 builds/html5/index.html
+
 npx surge builds/html5 saywhat-demo.surge.sh
